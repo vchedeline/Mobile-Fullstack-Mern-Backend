6b9ddbce08b2c0acd51c091d6ad899c880c3c5c2
chedeline <vchedeline@gmail.com>
2022-06-03 11:05:22 -0400

    connects to mongo

---
+++ config/connection.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE_URL);
+
+mongoose.connection
+  .on("error", (err) => console.log(err.message + "MongoDB Err?"))
+  .on("connected", () => console.log("We Are MongoDB Live!"))
+  .on("disconnected", () => console.log("No Longer Live MongoDB!"));
