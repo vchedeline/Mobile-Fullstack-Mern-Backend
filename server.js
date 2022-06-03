a928e0aa52eb112bc2101db4f592f7b8d9d48b3a
chedeline <vchedeline@gmail.com>
2022-06-03 10:59:51 -0400

    routes completed

--- server.js
+++ server.js
@@ -17,4 +17,48 @@ app.get("/", (req, res) => {
   res.send("I Am Home");
 });
 
+//IDUCS (from INDUCES)
+// Index
+app.get("/orders", async (req, res) => {
+  try {
+    res.json(await (await Orders.find({})).reverse());
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+// Delete
+app.delete("/orders/:id", async (req, res) => {
+  try {
+    res.json(await Orders.findByIdAndDelete(req.params.id));
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+// Update
+app.put("/orders/:id", async (req, res) => {
+  try {
+    res.json(
+      await Orders.findByIdAndUpdate(req.params.id, req.body, { new: true }),
+    );
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+// Create
+app.post("/orders", async (req, res) => {
+  try {
+    res.json(await Orders.create(req.body));
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+// Show
+app.get("/orders/:id", async (req, res) => {
+  try {
+    res.json(await Orders.findById(req.params.id));
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
 app.listen(PORT, () => console.log(`LIVE on Port ${PORT}!`));
