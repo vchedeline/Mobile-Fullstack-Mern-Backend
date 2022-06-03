d1721e7f44697081361a0de4a11fcf16122c048a
chedeline <vchedeline@gmail.com>
2022-06-03 10:55:24 -0400

    server setup

---
+++ server.js
@@ -0,0 +1,20 @@
+// DEPENDENCIES
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const { PORT = 8008 } = process.env;
+const app = express();
+
+// MIDDLEWARE
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
+// ROUTES
+// Home
+app.get("/", (req, res) => {
+  res.send("I Am Home");
+});
+
+app.listen(PORT, () => console.log(`LIVE on Port ${PORT}!`));
